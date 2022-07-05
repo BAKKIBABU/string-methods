a85f9f32e02b3f670ed1706e6ca7b3561f9eed96
BAKKIBABU <108388125+BAKKIBABU@users.noreply.github.com>
2022-07-05 17:53:25 +0530

    Add files via upload

---
+++ stringmethods1.py
@@ -0,0 +1,80 @@
+# a="hello world"
+# print(a.capitalize())  #Hello world
+# print('HELLO BYE'.iscaptalaize())   #  attribute error
+# b='hello ravi'
+# print(b.upper())  #upper HELLO RAVI
+# print(b.lower())   # hello ravi  lower 
+# b='hELlo Ravi'
+# print(b.upper())   #upper HELLO RAVI
+# print(b.lower())   # lower hello ravi
+# print(b.casefold())   #   case fold hello ravi
+# print(b.count())     #this type use count method type error
+# print('b'.isupper())  #false
+# print("hello ravi".isupper())   #false
+# print('hello ravi'.islower())     #truefier
+# print("HELLO RAVI".islower())    #False
+# print('HellO rAVi'.islower())     #false
+# print('HellO rAVi'.isupper())      #false
+# print('HELLO RAVI'.title())        # Hello ravi  title method
+# print('hello ravi'.title())     # Hello ravi  title method
+# print('hello ravi'.istitle())  # lower case not possible
+# print('HELLO RAVI'.istitle())      #false
+# print('Hello Ravi'.istitle())           #true
+# print('Hello goutham'.istitle())      #false
+# print('python Code'.swapcase())  #PYTHON cODE
+# print('PYTHON CODE'.swapcase())     #python code   swapcase
+# print('hello babu'.swapcase())         #  HELLO BABU   swapcase
+# print('computer'.isidentifier())     #true
+# print('7computer'.isidentifier())     #false
+# print('_computer'isidentifier())  # syntax error
+# print('_computer'.isidentifier())   #true
+
+# a='computer'
+# 'computer'.isidentifier()
+# print(a)
+
+# a='12345'
+# print(a.isnumeric())  #true
+# a='12345tcfdvg'
+# print(a.isnumeric())   #false
+
+# a='banana'
+# print(a.center(80))
+# a='hi my name is niharika,pravalika and niharika are best friends in institute, niharika favoriate food shawarma and chiken biryani'
+# print(a.count('niharika'))  # 3
+# print(a.count('z'))  # 0
+
+# a='hi my name is ravikumar.'
+# print(a.endswith('.'))             #true
+# print(a.endswith('ravikumar.'))   #true
+# print(a.endswith('ravikumar'))   #false
+
+# b='p\tr\ta\tv\ta\tl\ti\tk\ta'
+# print(b.expandtabs(0))  #pravalika
+# print(b.expandtabs(1)) # p r a v a l i k a
+# print(b.expandtabs(2)) # p r a v a l i k a
+# print(b.expandtabs(3)) # p  r  a  v  a  l  i  k  a
+# print(b.expandtabs(4)) # p   r   a   v   a   l   i   k   a   
+# print(b.expandtabs(5)) # p    r    a    v    a    l    i    k    a
+
+# a='shahed,babu and ravi are friends'
+# print(a.find('babu'))   #7
+# print(a.find('ravi'))
+# print(a.find('z'))    #-1
+# print(a.index('z'))      #value error
+# a='goutham company12'
+# # print(a.isalnum())        #false
+# print(a.isalnum('a'))
+
+# a='THIS IS word'
+# print(a.isupper())  #true
+# print(a.isupper())    #false
+
+# b='this word'
+# print(b.islower())    #true
+# b='THIS IS WORD'
+# print(b.islower())  # false
+
+                                                                                                                                                                                                  
+
+
